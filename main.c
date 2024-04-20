fa3292460a094e780d25d785f0c26958e2a6eded
Canchili <148684811+Canchili@users.noreply.github.com>
2024-04-20 13:22:23 +1000

    Update main.c

--- main.c
+++ main.c
@@ -12,20 +12,23 @@ struct humen {
   int year;
 };
 int main(void) {
+  int n;
+  printf("Введите кл-во человек");
+  scanf("%d", &n);
   struct humen m1[4], m2[4], m3[4];
   int i, j;
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < n; i++) {
     printf("введите данные о человеке\n");
     scanf("%s %s %d", m1[i].name, m1[i].surname, &m1[i].year);
     m2[i] = m1[i];
   }
   printf("\n");
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < n; i++) {
     printf("%s %s %d\n", m1[i].name, m1[i].surname, m1[i].year);
   }
   printf("\n");
-  for (i = 0; i < 4; i++) {
-    for (j = 0; j < 3; j++) {
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n - 1; j++) {
       if (m2[j].year > m2[j + 1].year) {
         m3[j] = m2[j];
         m2[j] = m2[j + 1];
@@ -33,7 +36,7 @@ int main(void) {
       }
     }
   }
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < n; i++) {
     printf("%s %s %d\n", m2[i].name, m2[i].surname, m2[i].year);
   }
 
